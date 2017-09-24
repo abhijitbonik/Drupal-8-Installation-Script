4308078099ab22d1790e1ab425697b940138a8ca
abhisgithub <abhijitbonik@hotmail.com>
2017-09-25 01:20:20 +0530

    defconf error solved

--- drupal_install.sh
+++ drupal_install.sh
@@ -2,7 +2,8 @@
 #making the gui promt of mysql instalation non interactive
 #and provide the password on debconf-set-selections
 export DEBIAN_FRONTEND="noninteractive"
-
+sudo -S <<< $1 apt-get update
+sudo -S <<< $1 apt-get install -y debconf
 sudo -S <<< $1 debconf-set-selections <<< "mysql-server mysql-server/root_password password root"
 sudo -S <<< $1 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password root"
 sudo -S <<< $1 apt-get update
